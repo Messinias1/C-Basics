d1995839c930b38d5d41292c7fe4cf40e34b3bc9
Carl Kakisis <Messinias1@gmail.com>
2019-09-02 13:57:49 -0400

    Add project files.

---
+++ BullDozer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BullDozer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dozer = new Bulldozer("Yellow", "Mini");
+
+            var dozer2 = new Bulldozer("Red", "Large");
+
+            Console.WriteLine("Bulldozer is: " + dozer.color);
+            Console.WriteLine("Bulldozer 2 is: " + dozer2.color);
+        }
+    }
+
+    public class Bulldozer
+    {
+        public Bulldozer (string _color, string _size)
+        {
+            this.color = _color;
+            this.size = _size;
+        }
+
+        public string color { get; set; }
+        public string size { get; set; }
+    }
+}
