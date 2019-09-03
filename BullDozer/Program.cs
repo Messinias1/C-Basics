206c7583fdcf3d58917f1092a7b228c30fa7423c
Carl Kakisis <Messinias1@gmail.com>
2019-09-03 12:54:09 -0400

    encapsulation example

--- BullDozer/Program.cs
+++ BullDozer/Program.cs
@@ -11,9 +11,9 @@ namespace BullDozer
         static void Main(string[] args)
         {
             var worker = new FactoryWorker();
-            worker.Count(4);
-            worker.Count(5);
-            worker.Count(4.5f);
+            var result = worker.AddNumbers(6, 6);
+
+            Console.WriteLine(result.ToString());
         }
     }
 
@@ -24,17 +24,10 @@ namespace BullDozer
 
         }
 
-        public void Count (int number)
+        public int AddNumbers(int num1, int num2)
         {
-            Total = Total + Convert.ToSingle(number);
-            Console.WriteLine(Total.ToString());
+            var result = num1 + num2;
+            return result;
         }
-
-        public void Count(float number)
-        {
-            Total = Total + number;
-            Console.WriteLine(Total.ToString());
-        }
-        public float Total { get; set; }
     }
 }
